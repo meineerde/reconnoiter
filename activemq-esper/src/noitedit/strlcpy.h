4a0d9692f2c35c02eb819844c0b9c4bd2d57ae1e
jesus <jesus@7e16921a-1b8a-68b2-a91d-dfb2e35db99f>
2009-10-24 14:21:48 +0000

    completed
    git-svn-id: https://labs.omniti.com/reconnoiter/branches/dev@1033 7e16921a-1b8a-68b2-a91d-dfb2e35db99f

--- activemq-esper/src/noitedit/strlcpy.h
+++
@@ -1,7 +0,0 @@
-#include "noit_config.h"
-#ifndef HAVE_STRLCPY
-size_t strlcpy(char *dst, const char *src, size_t size);
-#endif
-#ifndef HAVE_STRLCAT
-size_t strlcat(char *dst, const char *src, size_t size);
-#endif
